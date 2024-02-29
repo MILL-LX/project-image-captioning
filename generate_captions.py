2270690b9f3ac31a33155d7c792169fc489621a5
Mo Mobarak <mo@mobarak.com>
2024-02-29 23:54:47 +0000

    write output to captions.txt

--- generate_captions.py
+++ generate_captions.py
@@ -3,22 +3,28 @@ import glob
 from os import path
 from transformers import pipeline
 
-image_dir = "data/mill_fotos_teste"
+image_dir = 'data/mill_fotos_teste'
 
-models = ["Salesforce/blip-image-captioning-base", "Salesforce/blip-image-captioning-large", "nlpconnect/vit-gpt2-image-captioning"]
+models = ['Salesforce/blip-image-captioning-base', 
+          'Salesforce/blip-image-captioning-large', 
+          'nlpconnect/vit-gpt2-image-captioning']
+
 captioners = []
 for model in models:
-    captioners.append(pipeline("image-to-text", model=model, max_new_tokens=128))
+    captioners.append(pipeline('image-to-text', model=model, max_new_tokens=128))
 
-for image_file_path in glob.glob(image_dir + '/*'):
-    image__filename = path.basename(image_file_path)
+with open('captions.txt', 'w') as captions_file:
 
-    print("\n\n")
-    print(image__filename)
+    for image_file_path in glob.glob(image_dir + '/*'):
+        image__filename = path.basename(image_file_path)
 
-    for i in range(len(captioners)):
-        caption = captioners[i](image_file_path)[0]['generated_text']
-        print(f'\t{models[i]}: {caption}')
+        print(image__filename, file=captions_file)
+
+        for i in range(len(captioners)):
+            caption = captioners[i](image_file_path)[0]['generated_text']
+            print(f'\t{models[i]}: {caption}', file=captions_file)
+
+        print('\n', file=captions_file)
 
 
 
