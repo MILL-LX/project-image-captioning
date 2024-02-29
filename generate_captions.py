73e440b49307673601bc178b09cc10325347b10b
Mo Mobarak <mo@mobarak.com>
2024-02-29 19:14:36 +0000

    basic results for all images

--- generate_captions.py
+++ generate_captions.py
@@ -1,4 +1,13 @@
+
+import glob
 from transformers import pipeline
 
-captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
-print(captioner("data/mill_fotos_teste/DSC01280.JPG"))
+image_dir = "data/mill_fotos_teste"
+
+# captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
+# captioner = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+captioner = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+
+for image_filename in glob.glob(image_dir + '/*'):
+    print(captioner(image_filename))
+
