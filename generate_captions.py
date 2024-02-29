ebc508a348af18f58d99b867682f5482d4a8fa81
Mo Mobarak <mo@mobarak.com>
2024-02-29 23:29:07 +0000

    compare a few models

--- generate_captions.py
+++ generate_captions.py
@@ -1,13 +1,24 @@
 
 import glob
+from os import path
 from transformers import pipeline
 
 image_dir = "data/mill_fotos_teste"
 
-# captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
-# captioner = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
-captioner = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+models = ["Salesforce/blip-image-captioning-base", "Salesforce/blip-image-captioning-large", "nlpconnect/vit-gpt2-image-captioning"]
+captioners = []
+for model in models:
+    captioners.append(pipeline("image-to-text", model=model, max_new_tokens=128))
 
-for image_filename in glob.glob(image_dir + '/*'):
-    print(captioner(image_filename))
+for image_file_path in glob.glob(image_dir + '/*'):
+    image__filename = path.basename(image_file_path)
+
+    print("\n\n")
+    print(image__filename)
+
+    for i in range(len(captioners)):
+        caption = captioners[i](image_file_path)[0]['generated_text']
+        print(f'\t{models[i]}: {caption}')
+
+
 
