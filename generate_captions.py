8bb528746121ede02808ce23d837b437ebcc7f77
Mo Mobarak <mo@mobarak.com>
2024-03-04 16:12:36 +0000

    a little reorg, write out the md file

--- generate_captions.py
+++ generate_captions.py
@@ -1,10 +1,15 @@
-
 import glob
 from os import path
 import time
+
+from PIL import Image
+from PIL.ExifTags import TAGS
 from transformers import pipeline
 
-image_dir = 'data/mill_fotos_teste'
+from lib.util import write_caption_to_exif
+
+image_dir = 'data/input'
+output_dir = 'data/output'
 
 models = ['Salesforce/blip-image-captioning-base', 
           'Salesforce/blip-image-captioning-large', 
@@ -15,19 +20,22 @@ captioners = [pipeline('image-to-text', model=model, max_new_tokens=128) for mod
 finish = time.perf_counter()
 print(f'Model loading took {finish-start:0.4f} seconds.')
 
-with open('captions.txt', 'w') as captions_file:
+with open('captioned_images.md', 'w') as captions_file:
 
     for image_file_path in glob.glob(image_dir + '/*'):
         start = time.perf_counter()
-        image__filename = path.basename(image_file_path)
+        image_filename = path.basename(image_file_path)
 
-        print(image__filename, file=captions_file)
-
+        captions = ''
         for model, captioner in zip(models, captioners):
             caption = captioner(image_file_path)[0]['generated_text']
-            print(f'\t{model}: {caption}', file=captions_file)
+            captions += f'{model}: {caption}\n\n'
 
-        print('\n', file=captions_file)
+        captioned_image_file_path = write_caption_to_exif(image_file_path, output_dir, 'This is my file description') 
+        print(f'![]({captioned_image_file_path})\n\n{captions}\n', file=captions_file)
+
+
 
         finish = time.perf_counter()
         print(f'Image captioning took {finish-start:0.4f} seconds.')
+
