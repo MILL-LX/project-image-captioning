eaf46165fe9af3d588927353c381ee91ab9e6a49
Mo Mobarak <mo@mobarak.com>
2024-03-05 19:01:26 +0000

    some refactoring

--- generate_captions.py
+++ generate_captions.py
@@ -1,38 +1,48 @@
 import glob
 from os import path
-import time
-
 from PIL import Image
 from PIL.ExifTags import TAGS
 from transformers import pipeline
+from lib.util import write_caption_to_exif, timer, CaptionsWriter
 
-from lib.util import write_caption_to_exif
 
-image_dir = 'data/input'
-output_dir = 'data/output'
+@timer
+def load_models(models):
+    return [pipeline('image-to-text', model=model, max_new_tokens=64) for model in models]
 
-models = ['Salesforce/blip-image-captioning-base', 
-          'Salesforce/blip-image-captioning-large', 
-          'nlpconnect/vit-gpt2-image-captioning']
 
-start = time.perf_counter()
-captioners = [pipeline('image-to-text', model=model, max_new_tokens=64) for model in models]
-finish = time.perf_counter()
-print(f'Model loading took {finish-start:0.4f} seconds.')
+@timer
+def make_captions_for_image(models, captioners, image_file_path):
+    image_filename = path.basename(image_file_path)
 
-with open('captioned_images.md', 'w') as captions_file:
+    captions = ''
+
+    for model, captioner in zip(models, captioners):
+        caption = captioner(image_file_path)[0]['generated_text']
+        captions += f'{model}: {caption}\n\n'
+
+    return captions
+
+@timer
+def process_images(image_dir, output_dir, models, captions_writer):
+    captioners = load_models(models)
 
     for image_file_path in glob.glob(image_dir + '/*'):
-        start = time.perf_counter()
-        image_filename = path.basename(image_file_path)
-
-        captions = ''
-        for model, captioner in zip(models, captioners):
-            caption = captioner(image_file_path)[0]['generated_text']
-            captions += f'{model}: {caption}\n\n'
+        captions = make_captions_for_image(models, captioners, image_file_path)
 
         captioned_image_file_path = write_caption_to_exif(image_file_path, output_dir, captions) 
-        print(f'![]({captioned_image_file_path})\n\n{captions}\n', file=captions_file)
+        captions_writer.write_captions(captioned_image_file_path, captions)
 
-        finish = time.perf_counter()
-        print(f'Image captioning took {finish-start:0.4f} seconds.')
+@timer
+def main():
+    image_dir = 'data/input'
+    output_dir = 'data/output'
+    models = ['Salesforce/blip-image-captioning-base', 
+              'Salesforce/blip-image-captioning-large', 
+              'nlpconnect/vit-gpt2-image-captioning']
+
+    captions_writer = CaptionsWriter('captioned_images.md')
+    process_images(image_dir, output_dir, models, captions_writer)
+
+if __name__ == "__main__":
+    main()
--- lib/util.py
+++ lib/util.py
@@ -1,5 +1,6 @@
 import os.path
 from pathlib import Path
+import time
 
 import piexif
 import piexif.helper
@@ -40,3 +41,20 @@ def write_caption_to_exif(input_image_path, output_dir, caption):
 
     except Exception as e:
         print(f'An error occurred writing caption to EXIF data: {e}')
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        print(f'{func.__name__} took {end_time - start_time:0.4f} seconds.')
+        return result
+    return wrapper
+
+class CaptionsWriter:
+    def __init__(self, captions_file_path):
+        self.captions_file_path = captions_file_path
+
+    def write_captions(self, captioned_image_path, captions):
+        with open(self.captions_file_path, 'a') as captions_file:
+            print(f'![]({captioned_image_path})\n\n{captions}\n', file=captions_file)
